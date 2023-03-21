a5817bf9442a46dd0d0f8b602b7bd75c46b905f4
Drew McPherson <drewmac1971@gmail.com>
2023-03-21 14:54:12 -0500

    destructuring assignment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -37,20 +37,20 @@ const Footer = () => {
                     </Col>
                     <Col sm='4' className='text-center'>
                         <a
-                                role='button'
-                                className='btn btn-link'
-                                href='tel:+12065551234'
-                            >
-                                <i className='fa fa-phone' /> 1-206-555-1234
-                            </a>
-                            <br />
-                            <a
-                                role='button'
-                                className='btn btn-link'
-                                href='mailto:notreal@notreal.co'
-                            >
-                                <i className='fa fa-envelope-o' /> campsites@nucamp.co
-                            </a>                
+                            role='button'
+                            className='btn btn-link'
+                            href='tel:+12065551234'
+                        >
+                            <i className='fa fa-phone' /> 1-206-555-1234
+                        </a>
+                        <br />
+                        <a
+                            role='button'
+                            className='btn btn-link'
+                            href='mailto:notreal@notreal.co'
+                        >
+                            <i className='fa fa-envelope-o' /> campsites@nucamp.co
+                        </a>
                     </Col>
                 </Row>
             </Container>
