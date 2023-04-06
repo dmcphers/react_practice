d7ece8e7f7174744922de56f806ae1dcc237b40a
Drew McPherson <drewmac1971@gmail.com>
2023-04-05 21:11:16 -0500

    Redux for All

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import { campsitesReducer } from '../features/campsites/campsitesSlice';
+import { commentsReducer } from '../features/comments/commentsSlice';
+import { promotionsReducer } from '../features/promotions/promotionsSlice';
+import { partnersReducer } from '../features/partners/partnersSlice';
 
 export const store = configureStore({
   reducer: {
-    campsites: campsitesReducer
+    campsites: campsitesReducer,
+    comments: commentsReducer,
+    partners: partnersReducer,
+    promotions: promotionsReducer
   },
 });
