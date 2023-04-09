63b49bf2c5c10e735533e8bc5576c991f7f48ef1
Drew McPherson <drewmac1971@gmail.com>
2023-04-09 12:51:24 -0500

    workshop 4

--- src/app/store.js
+++ src/app/store.js
@@ -5,13 +5,15 @@ import { commentsReducer } from '../features/comments/commentsSlice';
 import { promotionsReducer } from '../features/promotions/promotionsSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
 import logger from 'redux-logger';
+import { userReducer } from '../features/user/userSlice';
 
 export const store = configureStore({
   reducer: {
     campsites: campsitesReducer,
     comments: commentsReducer,
     partners: partnersReducer,
-    promotions: promotionsReducer
+    promotions: promotionsReducer,
+    user: userReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
 });
